6154c6593e4762d0461dba5c663ed455f1a36914
Mughees Mehdi <mugheesmehdi@gmail.com>
2024-03-03 06:08:34 +0500

    init

---
+++ Week 2/file_handling.py
@@ -0,0 +1,50 @@
+# Create a sample file (sample.txt) with some text in it before running this code
+import os
+
+
+# Open the file in read mode
+# file_path = 'test_file.txt'
+# with open(file_path, 'r') as file:
+#     # Read the entire contents of the file
+#     content = file.read()
+#     print("File Contents:")
+#     print(content)
+
+# Output:
+# File Contents:
+# This is a sample text file.
+# It contains some lines of text.
+# We will read this file using Python.
+
+
+#Open a file in write mode
+# file_path = 'output1.txt'
+# with open(file_path, 'w') as file:
+#     # Write to the file
+#     file.write("This is written to the file.\n")
+#     file.write("Here is another line of text.\n")
+#     file.write("Python file handling is easy!")
+#
+# print("File 'output.txt' has been written successfully.")
+#
+#
+# # Open a file in append mode
+# file_path = 'output1.txt'
+# with open(file_path, 'a') as file:
+#     # Append to the file
+#     file.write("\nAppending more text to the file.\n")
+#     file.write("This will appear after the existing content.")
+#
+# print("File 'output.txt' has been appended successfully.")
+
+
+# if os.path.exists("output1.txt"):
+#   os.remove("output1.txt")
+# else:
+#   print("The file does not exist")
+
+
+
+
+
+
