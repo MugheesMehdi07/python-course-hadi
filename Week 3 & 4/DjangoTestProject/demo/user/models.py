78df23d574e81bb3fbee82b8c17238240de6a118
Mughees Mehdi <mugheesmehdi@gmail.com>
2024-03-06 17:46:51 +0500

    project add

---
+++ Week 3 & 4/DjangoTestProject/demo/user/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class UserProfile(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    password = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+
